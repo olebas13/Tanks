8e9ce64969c8bcef0724532a5ae4b93c8c34ae07
olebas13 <o.nevoyt13@gmail.com>
2019-01-15 18:49:22 +0200

    create display and main class

---
+++ src/com/olebas/tanks/main/Main.java
@@ -0,0 +1,10 @@
+package com.olebas.tanks.main;
+
+import com.olebas.tanks.display.Display;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Display.create(800, 600, "Tanks");
+    }
+}
