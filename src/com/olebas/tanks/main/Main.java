24e915237766e33ffc6ca905f71d9b8a376ad80c
olebas13 <o.nevoyt13@gmail.com>
2019-01-16 12:41:34 +0200

    add render methods

--- src/com/olebas/tanks/main/Main.java
+++ src/com/olebas/tanks/main/Main.java
@@ -2,9 +2,22 @@ package com.olebas.tanks.main;
 
 import com.olebas.tanks.display.Display;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
 public class Main {
 
     public static void main(String[] args) {
         Display.create(800, 600, "Tanks");
+
+        Timer t = new Timer(1000 / 60, new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Display.render();
+            }
+        });
+
+        t.setRepeats(true);
+        t.start();
     }
 }
