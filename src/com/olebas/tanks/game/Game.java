9d479bbb29beed6720f4c154e94a67d38b724b03
olebas13 <o.nevoyt13@gmail.com>
2019-01-17 17:24:14 +0200

    add resource loader

--- src/com/olebas/tanks/game/Game.java
+++ src/com/olebas/tanks/game/Game.java
@@ -2,6 +2,7 @@ package com.olebas.tanks.game;
 
 import com.olebas.tanks.IO.Input;
 import com.olebas.tanks.display.Display;
+import com.olebas.tanks.graphics.TextureAtlas;
 import com.olebas.tanks.utils.Time;
 
 import java.awt.*;
@@ -18,11 +19,13 @@ public class Game implements Runnable {
     public static final float UPDATE_RATE = 60.0f;
     public static final float UPDATE_INTERVAL = Time.SECOND / UPDATE_RATE;
     public static final long IDLE_TIME = 1;
+    public static final String ATLAS_FILE_NAME = "texture_atlas.png";
 
     private boolean running;
     private Thread gameThread;
     private Graphics2D graphics;
     private Input input;
+    private TextureAtlas atlas;
 
     // temp
     float x = 350;
@@ -39,6 +42,7 @@ public class Game implements Runnable {
         graphics = Display.getGraphics();
         input = new Input();
         Display.addInputListener(input);
+        atlas = new TextureAtlas(ATLAS_FILE_NAME);
     }
 
     public synchronized void start() {
@@ -81,8 +85,11 @@ public class Game implements Runnable {
     private void render() {
         Display.clear();
         graphics.setColor(Color.white);
-        graphics.fillOval((int)(x + (Math.sin(delta) * 200)), (int)y, (int)radius * 2, (int)radius * 2);
+        graphics.drawImage(atlas.cut(0,0, 16, 16), 300, 300, null);
+        //graphics.fillOval((int)(x + (Math.sin(delta) * 200)), (int)y, (int)radius * 2, (int)radius * 2);
         Display.swapBuffers();
+
+
     }
 
     @Override
